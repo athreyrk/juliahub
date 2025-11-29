1c9381986bcda0d958f4fd100b283cdf7228055d
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2025-11-29 18:59:47 +0530

    Add bottom chamber equations separately, change initial p&T (!)
    - Used compose to connect top and bottom
    - Slightly increased initial pressure and temperature of both chambers
      above atmospheric
    
    ! Only did so because keeping initial conditions reliant solely on
      guesses makes it an underdetermined initial system, making it very
      sensitive - maybe there's a better way

--- airmotor.jl
+++ airmotor.jl
@@ -30,11 +30,11 @@ rhostar = p0/(R*T0) * (2/(k+1))^(1/(k-1))
 tmid = (L - 2*S) / V
 
 vars = @variables begin
-    p(t) = patm
-    T(t)
+    p(t) = patm + 10
+    T(t) = Tatm + 1e-3
     vol(t)
     m(t) = p * vol / (R * T)
-    Twall(t) = Tatm
+    Twall(t)# = Tatm
     Me(t)
     Te(t)
     rhoe(t)
@@ -45,11 +45,6 @@ vars = @variables begin
     tr(t)
     pe(t)
 end
-
-initvals = Dict([
-    Te => Tstar
-    tr => T0/Tstar
-    ])
 
 function smooth_transition(in, mid, lo, hi; steepness = 100)
     # if in < mid, lo, else, hi, end
@@ -124,21 +119,141 @@ eqs = [
     Q ~ -pi * D * x * hint * (T - Twall)
 
     # rate of heat "loss" from entire wall to atmosphere (same Q because wall doesn't store heat)
-    -Q ~ hext * pi * D * L * (Twall - Tatm)
+    # -Q ~ hext * pi * D * L * (Twall - Tatm)
 
     # energy balance (dU/dT = m(cp - h/T) = 0)
     m*(cv*T*der(p)/p + cp*T*der(vol)/vol) ~ der(m)*(cp*Te + 1/2 * Ve^2) + Q
     
 ]
 
-@mtkcompile sys = System(eqs, t, vars, pars)
+@named top = System(eqs, t, vars, pars)
 
-prob = ODEProblem(sys, [], (0.0, 3.6); fully_determined = true, guesses = initvals)
+btmeqs = [
+    # derivative of ideal gas law (same as equation of mass balance)
+    der(p) / p + der(vol) / vol ~ der(m) / m + der(T) / T
+
+    # motion of cylinder (defines volume change)
+    # x ~ ifelse(t < tmid,
+    #     S + V*t
+    #     ,
+    #     L - S - V*(t-tmid)
+    #     )
+    
+    vol ~ A * x
+
+    # mach number at inlet/ exit (choked at 1 for pressure ratio more than critical)
+    Me ~ ifelse(pr > prCrit, 
+        1.0
+        ,
+        sqrt(2/(k-1) * ((pr)^((k-1)/k) - 1))
+        )
+    
+    # definition of pressure ratio
+    pr ~ ifelse(t > tmid,
+        p0/p
+        ,
+        p/patm
+        )
+
+    # definition of temperature ratio
+    tr ~ ifelse(t > tmid,
+        T0/Te
+        ,
+        T/Te
+        )
+
+    # isentropic flow at the inlet/ exit
+    tr ^ (k/(k-1)) ~ ifelse(t > tmid,
+        p0/pe
+        ,
+        p/pe
+        )
+
+    # ideal gas law at the inlet/ exit
+    R * rhoe * Te ~ pe
+
+    pe ~ smooth_transition(t, tmid,
+            patm,
+            ifelse(pr > prCrit,
+                p0/prCrit, # critical pressure w.r.t. reservoir
+                p) # same as inside
+            # , # e denotes exit
+            # ifelse(pr > prCrit,
+            #     p/prCrit, # critical pressure w.r.t. atmosphere
+            #     patm) # same as outside
+        )
+    
+    # velocity at inlet/ exit
+    Ve ~ Me * sqrt(k * R * Te)
+
+    # mass flow rate at inlet/ exit
+    der(m) ~ ifelse(t > tmid,
+        rhoe * Ae * Ve
+        ,
+        -rhoe * Ae * Ve
+        )
+
+    # rate of heat "addition" from part of wall in contact with top chamber
+    Q ~ -pi * D * x * hint * (T - Twall)
+
+    # rate of heat "loss" from entire wall to atmosphere (same Q because wall doesn't store heat)
+    # -Q ~ hext * pi * D * L * (Twall - Tatm)
+
+    # energy balance (dU/dT = m(cp - h/T) = 0)
+    m*(cv*T*der(p)/p + cp*T*der(vol)/vol) ~ der(m)*(cp*Te + 1/2 * Ve^2) + Q
+    
+]
+
+@named btm = System(btmeqs, t, vars, pars)
+
+fulleqs = [
+    top.x + btm.x ~ L
+    top.Twall ~ btm.Twall
+    top.Q + btm.Q + top.hext * pi * top.D * top.L * (top.Twall - Tatm) ~ 0
+]
+# fulleqs = [
+#     btm.x ~ L -
+#     ifelse(t < tmid,
+#         S + V*t
+#         ,
+#         L - S - V*(t-tmid)
+#         )
+#     # top.Twall ~ btm.Twall
+#     btm.Q + btm.hext * pi * btm.D * btm.L * (btm.Twall - Tatm) ~ 0
+# ]
+
+@named full = System(fulleqs, t)
+@mtkcompile sys = compose(full, top, btm)
+# @mtkcompile sys = compose(full, btm)
+
+# @mtkcompile sys = System(eqs, t, vars, pars)
+initvals = Dict([
+    top.Te => substitute(Tstar, [T0 => top.T0])
+    top.tr => T0/Tstar
+    btm.Te => Tatm
+    btm.tr => 1
+    # top.p => patm
+    # btm.p => patm
+    # top.T => Tatm
+    # btm.T => Tatm
+    # btm.Twall => Tatm
+    # top.m => substitute(patm * A * S / (R * Tatm), [D => top.D, S => top.S])
+    # btm.m => substitute(patm * A * (L-S) / (R * Tatm), [D => btm.D, S => btm.S])
+    # top.rhoe => substitute(rhostar, [p0 => top.p0, T0 => top.T0])
+    # btm.rhoe => patm/ (R * Tatm)
+    ])
+
+prob = ODEProblem(sys, [], (0.0, 1.2); fully_determined = true, guesses = initvals)#, warn_cyclic_dependency = true, substitution_limit = 50)
+# prob = ODEProblem(sys, [], (0.0, 1.2); fully_determined = false, guesses = initvals)#, warn_cyclic_dependency = true, substitution_limit = 50)
 sol = solve(prob)
+# alwaysFalse(a...) = false
+# sol = solve(prob; adaptive = false, dt = 1e-10, abstol = 1e-4, reltol = 1e-3)
+# sol = solve(prob; adaptive = false, dt = 1e-7, unstable_check = alwaysFalse, force_dtmin = true, abstol = 1e-2, reltol = 1e-1)
+# sol = solve(prob; dtmin = 1e-3, unstable_check = alwaysFalse, force_dtmin = true, abstol = 1e-2, reltol = 1e-1)
 
-display(plot(sol[t], sol[T-273.15], xlabel="sec", ylabel="degC", label="T"))
-display(plot(sol[t], sol[p], xlabel="sec", ylabel="Pa", label="p"))
-display(plot(sol[t], sol[m], xlabel="sec", ylabel="kg", label="m"))
-display(plot(sol[t], sol[m/vol], xlabel="sec", ylabel="kg/m^3", label="rho"))
-display(plot(sol[t], sol[Twall-273.15], xlabel="sec", ylabel="degC", label="Twall"))
-display(plot(sol[t], sol[-Q], xlabel="sec", ylabel="W", label="-Q", title="Rate of heat transfer from gas to wall"))
+display(plot(sol[t], sol[top.T-273.15], xlabel="sec", ylabel="degC", label="T"))
+# display(plot(sol[t], sol[btm.p], xlabel="sec", ylabel="Pa", label="p"))
+# display(plot(sol[t], sol[btm.m], xlabel="sec", ylabel="kg", label="m"))
+# display(plot(sol[t], sol[btm.m/btm.vol], xlabel="sec", ylabel="kg/m^3", label="rho"))
+# display(plot(sol[t], sol[btm.Twall-273.15], xlabel="sec", ylabel="degC", label="Twall"))
+# display(plot(sol[t], sol[-btm.Q], xlabel="sec", ylabel="W", label="-Q", title="Rate of heat transfer from gas to wall"))
