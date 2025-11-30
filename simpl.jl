705fcda30e018df8d31d77875f975f7cd9b0b869
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2025-11-30 19:30:19 +0530

    Add MWE of problems related to discrete callbacks

--- simpl.jl
+++ simpl.jl
@@ -1,40 +1,182 @@
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as der
-# using OrdinaryDiffEq
 using DifferentialEquations
-# using Plots
+using Plots
 
-# R = 287.0
+patm = 101325.0
+k = 1.4
+R = 287.0
+Tatm = 25 + 273.15 # normal temperature
+prCrit = ((k+1)/2)^(k/(k-1))
+cp = 1005.0
+cv = cp / k
 
 pars = @parameters begin
-    # p = 101325.0
-    # m = 0.0046
-    # A = pi/4 * (3/39.37)^2
+    p0 = 100 * 6894.76 + patm
+    T0 = 25 + 273.15
+    D = 3 / 39.37
+    L = 4 / 39.37
+    de = 10 / 1000
     S = 0.2 / 39.37
     V = 2 / 39.37
-    L = 4 / 39.37
+    hint = 100.0
+    hext = 5.0
 end
 
-tmid = (L - 2*S)/V
+Ae = pi/4 * de^2
+A = pi/4 * D^2
+Tstar = T0 * (2/(k+1))
+rhostar = p0/(R*T0) * (2/(k+1))^(1/(k-1))
+tmid = (L - 2*S) / V
 
 vars = @variables begin
-    x(t) = S
-    # T(t)
+    p(t) = patm
+    T(t)
+    vol(t)
+    m(t) = p * vol / (R * T)
+    Twall(t) = Tatm
+    Me(t)
+    Te(t)
+    rhoe(t)
+    Q(t)
+    x(t)
+    Ve(t)
+    pr(t)
+    tr(t)
+    pe(t)
+    direction(t)
 end
 
-# vol = A*x
+initvals = Dict([
+    Te => Tstar
+    tr => T0/Tstar
+    ])
 
 eqs = [
-    # p * vol ~ m * R * T
-    der(x) ~ V
-    ]
+    # derivative of ideal gas law (same as equation of mass balance)
+    der(p) / p + der(vol) / vol ~ der(m) / m + der(T) / T
 
-@mtkcompile sys = System(eqs, t, vars, pars)
+    # motion of cylinder (defines volume change)
+    x ~ 
+    ifelse(t <= tmid,
+        S + V*t
+        ,
+        L - S - V*(t-tmid)
+        )
+    
+    vol ~ A * x
 
-prob_working = ODEProblem(sys, [], (0.0, 10.0)) # does work
-prob_nowork = ODEProblem(sys, [], (0.0, tmid)) # doesn't work
+    # mach number at inlet/ exit (choked at 1 for pressure ratio more than critical)
+    Me ~ ifelse(pr >= prCrit, 
+        1.0
+        ,
+        ifelse(pr < prCrit,
+            sqrt(2/(k-1) * ((pr)^((k-1)/k) - 1)),
+            NaN)
+        )
+    
+    # definition of pressure ratio
+    pr ~ 
+    ifelse(t <= tmid,
+        p0/p
+        ,
+        p/patm
+        )
 
-sol_working = solve(prob_working)
-sol_err = solve(prob_nowork)
+    # definition of temperature ratio
+    tr ~ 
+    ifelse(t <= tmid,
+        T0/Te
+        ,
+        T/Te
+        )
 
-# plot(sol, idxs = [x])
+    # isentropic flow at the inlet/ exit
+    tr ^ (k/(k-1)) ~ 
+    ifelse(t <= tmid,
+        p0/pe
+        ,
+        p/pe
+        )
+
+    # ideal gas law at the inlet/ exit
+    R * rhoe * Te ~ pe
+
+    pe ~
+    ifelse(t <= tmid,
+        # e denotes inlet
+        ifelse(pr >= prCrit,
+            p0/prCrit, # critical pressure w.r.t. reservoir
+            ifelse(pr < prCrit,
+                p, # same as inside
+                NaN
+            )
+        ), # e denotes exit
+        ifelse(pr >= prCrit,
+            p/prCrit, # critical pressure w.r.t. atmosphere
+            ifelse(pr < prCrit,
+                patm, # same as outside
+                NaN
+            )
+        )
+    )
+    
+    # velocity at inlet/ exit
+    Ve ~ Me * sqrt(k * R * Te)
+
+    # mass flow rate at inlet/ exit
+    der(m) ~ rhoe * Ae * Ve * direction
+
+    # rate of heat "addition" from part of wall in contact with top chamber
+    Q ~ -pi * D * x * hint * (T - Twall)
+
+    # rate of heat "loss" from entire wall to atmosphere (same Q because wall doesn't store heat)
+    -Q ~ hext * pi * D * L * (Twall - Tatm)
+
+    # energy balance (dU/dT = m(cp - h/T) = 0)
+    m*(cv*T*der(p)/p + cp*T*der(vol)/vol) ~ der(m)*(cp*Te + 1/2 * Ve^2) + Q
+
+    direction ~ 
+        ifelse(t<= tmid,
+            1,
+            -1
+    )
+    
+]
+
+switching = 
+    [1.8] => [
+    p ~ Pre(p)
+    T ~ Pre(T)
+    # vol ~ Pre(vol)
+    m ~ Pre(m)
+    # Twall ~ Pre(Twall)
+    # Me ~ 1.0
+    # Te ~ Tstar
+    # rhoe ~ rhostar
+    # Q ~ Pre(Q)
+    # x ~ Pre(x)
+    # Ve ~ sqrt(k*R*Tstar)
+    # pr ~ p/patm
+    # tr ~ T/Te
+    # pe ~ p/prCrit
+    # direction ~ -1.0
+]
+
+# switching = ModelingToolkit.SymbolicDiscreteCallback(
+#     [1.8] => [
+#     direction ~ -1.0
+# ];
+# discrete_parameters = direction, iv = t)
+
+@mtkcompile sys = System(eqs, t, vars, pars; discrete_events = switching)
+
+prob = ODEProblem(sys, [], (0.0, 3.6); fully_determined = true, guesses = initvals)
+sol = solve(prob, Rosenbrock23(), force_dtmin = true)
+
+# display(plot(sol[t], sol[T-273.15], xlabel="sec", ylabel="degC", label="top.T"))
+# display(plot(sol[t], sol[p], xlabel="sec", ylabel="Pa", label="top.p"))
+# display(plot(sol[t], sol[m], xlabel="sec", ylabel="kg", label="top.m"))
+# display(plot(sol[t], sol[m/vol], xlabel="sec", ylabel="kg/m^3", label="top.rho"))
+# display(plot(sol[t], sol[Twall-273.15], xlabel="sec", ylabel="degC", label="Twall"))
+# display(plot(sol[t], sol[-Q], xlabel="sec", ylabel="W", label="-top.Q", title="Rate of heat transfer from top gas to wall"))
