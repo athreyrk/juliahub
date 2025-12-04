0ab26fcaf3ca526cd4638ee4bb801ceea4e207e3
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2025-12-04 19:14:57 +0530

    Consider thermal mass in heat transfer, add doc and plots

--- airmotor.jl
+++ airmotor.jl
@@ -3,74 +3,58 @@ using ModelingToolkit: t_nounits as t, D_nounits as der
 using DifferentialEquations
 using Plots
 
-patm = 101325.0
-k = 1.4
-R = 287.0
-Tatm = 25 + 273.15 # normal temperature
-prCrit = ((k+1)/2)^(k/(k-1))
-cp = 1005.0
+patm = 101325.0 # Pa
+k = 1.4 # gamma
+R = 287.0 # J/K
+Tatm = 25 + 273.15 # K
+prCrit = ((k+1)/2)^(k/(k-1)) # min pressure ratio for choked flow
+cp = 1005.0 # J/kg-K
 cv = cp / k
 
 pars = @parameters begin
-    p0 = 100 * 6894.76 + patm
-    T0 = 25 + 273.15
-    D = 3 / 39.37
-    L = 4 / 39.37
-    de = 10 / 1000
-    S = 0.2 / 39.37
-    V = 2 / 39.37
-    hint = 100.0
-    hext = 5.0
+    p0 = 100 * 6894.76 + patm # Pa
+    T0 = 25 + 273.15 # K
+    D = 3 / 39.37 # m
+    L = 4 / 39.37 # m
+    de = 10 / 1000 # m
+    S = 0.2 / 39.37 # m
+    V = 2 / 39.37 # m/s
+    hint = 100.0 # W/m2-K
+    hext = 5.0 # W/m2-K
+    Cpwall = 2 * 500 # J/K, non-specific heat capacity of wall
 end
 
-Ae = pi/4 * de^2
-A = pi/4 * D^2
-Tstar = T0 * (2/(k+1))
-rhostar = p0/(R*T0) * (2/(k+1))^(1/(k-1))
-tmid = (L - 2*S) / V
+Ae = pi/4 * de^2 # m2, area of inlet/exit
+A = pi/4 * D^2 # m2, area of piston
+Tstar = T0 * (2/(k+1)) # critical temperature w.r.t. reservoir
+tmid = (L - 2*S) / V # time at which spool switches
 
 vars = @variables begin
     p(t)[1:2] = [patm + 7; patm + 7]
     T(t)[1:2] = [Tatm + 1e-3; Tatm + 1e-3]
     vol(t)[1:2]
     m(t)[1:2] = [p[1] * vol[1] / (R * T[1]); p[2] * vol[2] / (R * T[2])]
-    Twall(t)#[1:2]# = Tatm
-    Me(t)[1:2]
-    Te(t)[1:2]
-    rhoe(t)[1:2]
-    Q(t)[1:2]# = 0
+    Twall(t) = Tatm
+    Me(t)[1:2] # mach number at inlet/ exit
+    Te(t)[1:2] # temperature at inlet/ exit
+    rhoe(t)[1:2] # density at inlet/ exit
+    Q(t)[1:2] # rate of heat transfer FROM wall into chambers
     x(t)[1:2]
-    Ve(t)[1:2]
-    pr(t)[1:2]
-    tr(t)[1:2]
-    pe(t)[1:2]
+    Ve(t)[1:2] # velocity at inlet/ exit
+    pr(t)[1:2] # ratio of pressures across inlet/ exit
+    tr(t)[1:2] # temperature ratio across inlet/ exit
+    pe(t)[1:2] # pressure at inlet/ exit
 end
 
+# define dictionary of initial guesses to replace symbols in initialization system
 initvals = Dict([
-    Te => [Tstar;Tatm]
-    tr => [T0/Tstar;1]
-    # T[2] => Tatm
+    Te => [Tstar; Tatm]
+    tr => [T0/Tstar; 1]
     ])
-
-function smooth_transition(in, mid, lo, hi; steepness = 100)
-    # if in < mid, lo, else, hi, end
-    (hi-lo)/2 * tanh(steepness*(in-mid)) + (hi+lo)/2
-end
 
 eqs = [
     # derivative of ideal gas law (same as equation of mass balance)
     der.(p) ./ p + der.(vol) ./ vol ~ der.(m) ./ m + der.(T) ./ T
-    # der(p)[1] / p[1] + der(vol)[1] / vol[1] ~ der(m)[1] / m[1] + der(T)[1] / T[1]
-    # if t < tmid
-    #     der(p)[2] ~ 0
-    #     der(T)[2] ~ 0
-    # else
-    #     der(p)[2] / p[2] + der(vol)[2] / vol[2] ~ der(m)[2] / m[2] + der(T)[2] / T[2]
-    # end
-    # 0 ~ ifelse(t < tmid,
-    #     der(T)[2],
-    #     der(p)[2] / p[2] + der(vol)[2] / vol[2] - der(m)[2] / m[2] - der(T)[2] / T[2]
-    # )
 
     # motion of cylinder (defines volume change)
     x[1] ~ ifelse(t < tmid,
@@ -83,7 +67,7 @@ eqs = [
 
     # mach number at inlet/ exit (choked at 1 for pressure ratio more than critical)
     Me ~ ifelse.(pr .> prCrit, 
-        [1.0;1.0]
+        [1.0; 1.0]
         ,
         sqrt.(2/(k-1) .* ((pr).^((k-1)/k) .- 1))
     )
@@ -129,7 +113,7 @@ eqs = [
 
     pe[1] ~ 
     ifelse(t < tmid,
-    # smooth_transition(t, tmid,
+        # e denotes inlet
         ifelse(pr[1] > prCrit,
             p0/prCrit, # critical pressure w.r.t. reservoir
             p[1]) # same as inside
@@ -140,7 +124,7 @@ eqs = [
     )
     pe[2] ~ 
     ifelse(t < tmid,
-    # smooth_transition(t, tmid,
+        # e denotes inlet
             patm # same as outside
         , # e denotes exit
         ifelse(pr[2] > prCrit,
@@ -166,25 +150,29 @@ eqs = [
     # rate of heat "addition" from part of wall in contact with top chamber
     Q ~ -pi .* D .* x .* hint .* (T .- Twall)
 
-    # rate of heat "loss" from entire wall (including both chambers and atmosphere) is 0 (because wall doesn't store heat)
-    Q[1] + Q[2] + hext * pi * D * L * (Twall - Tatm) ~ 0
+    # rate of heat "loss" from entire wall (including both chambers, atmosphere, and thermal mass) is 0 (because wall doesn't store heat)
+    Q[1] + Q[2] + Cpwall * der(Twall) + hext * pi * D * L * (Twall - Tatm) ~ 0
     # Q ~ -hext * pi * D * L .* (Twall .- Tatm)
 
     # energy balance (dU/dT = m(cp - h/T) = 0)
-    m.*(cv.*T.*der.(p)./p + cp.*T.*der.(vol)./vol) ~ der.(m).*(cp.*Te + 1/2 .* Ve.^2) .+ Q
-    # m[1]*(cv*T[1]*der(p)[1]/p[1] + cp*T[1]*der(vol)[1]/vol[1]) ~ der(m)[1]*(cp*Te[1] + 1/2 * Ve[1]^2) + Q[1]
+    m.*(cv.*T.*der.(p)./p .+ cp.*T.*der.(vol)./vol) ~ der.(m).*(cp.*Te + 1/2 .* Ve.^2) .+ Q
     
 ]
 
 @mtkcompile sys = System(eqs, t, vars, pars)
 
-prob = ODEProblem(sys, [], (0.0, 3.6); fully_determined = true, guesses = initvals)#, substitution_limit = 5, warn_cyclic_dependency = true)
-# alwaysFalse(a...) = false
-sol = solve(prob, Rodas5(), force_dtmin = true)#, unstable_check = alwaysFalse, force_dtmin = true)
+prob = ODEProblem(sys, [], (0.0, 3.6); fully_determined = true, guesses = initvals)
 
-# display(plot(sol[t], sol[T[1]-273.15], xlabel="sec", ylabel="degC", label="T"))
-# display(plot(sol[t], sol[p], xlabel="sec", ylabel="Pa", label="p"))
-# display(plot(sol[t], sol[m], xlabel="sec", ylabel="kg", label="m"))
-# display(plot(sol[t], sol[m/vol], xlabel="sec", ylabel="kg/m^3", label="rho"))
-# display(plot(sol[t], sol[Twall-273.15], xlabel="sec", ylabel="degC", label="Twall"))
-# display(plot(sol[t], sol[-Q], xlabel="sec", ylabel="W", label="-Q", title="Rate of heat transfer from gas to wall"))
+sol = solve(prob, Rodas5(), force_dtmin = true)
+
+display(plot(sol[t], sol[T[1]-273.15], xlabel="sec", ylabel="degC", label="top T"))
+display(plot(sol[t], sol[p[1]], xlabel="sec", ylabel="Pa", label="top p"))
+display(plot(sol[t], sol[m[1]], xlabel="sec", ylabel="kg", label="top m"))
+display(plot(sol[t], sol[m[1]/vol[1]], xlabel="sec", ylabel="kg/m^3", label="top rho"))
+display(plot(sol[t], sol[Twall-273.15], xlabel="sec", ylabel="degC", label="Twall"))
+# display(plot(sol[t], sol[-Q[1]], xlabel="sec", ylabel="W", label="top -Q", title="Rate of heat transfer from gas in top chamber to wall"))
+# display(plot(sol[t], sol[T[2]-273.15], xlabel="sec", ylabel="degC", label="btm T"))
+# display(plot(sol[t], sol[p[2]], xlabel="sec", ylabel="Pa", label="btm p"))
+# display(plot(sol[t], sol[m[2]], xlabel="sec", ylabel="kg", label="btm m"))
+# display(plot(sol[t], sol[m[2]/vol[2]], xlabel="sec", ylabel="kg/m^3", label="btm rho"))
+# display(plot(sol[t], sol[-Q[2]], xlabel="sec", ylabel="W", label="btm -Q", title="Rate of heat transfer from gas in bottom chamber to wall"))
